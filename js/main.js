bf8b1930a2fa40c1d21af3a11c2737061d64460d
Dawid <dawid.marchewczyk@gmail.com>
2023-11-14 02:03:05 +0100

    created a company website

---
+++ js/main.js
@@ -0,0 +1,49 @@
+const faqItems = document.querySelectorAll('.faq-item');
+const faqBtns = document.querySelectorAll('.faq-item__btn');
+const navBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.nav');
+const navLinks = document.querySelectorAll('.nav-links__item a');
+
+const currYear = new Date().getFullYear();
+const footerYear = (document.querySelector('.footer-year').textContent = currYear);
+
+let menuOpen = false;
+
+const handleNav = () => {
+	if (!menuOpen) {
+		navBtn.classList.add('open');
+		menu.classList.add('open');
+		document.body.style.overflow = 'hidden';
+		menuOpen = true;
+	} else {
+		navBtn.classList.remove('open');
+		menu.classList.remove('open');
+		document.body.style.overflow = 'visible';
+		menuOpen = false;
+	}
+};
+
+const removeActiveFaqItems = () => {
+	faqItems.forEach((faqItem) => faqItem.classList.remove('active'));
+};
+
+const toggleActiveFaqItems = () => {
+	removeActiveFaqItems();
+};
+
+faqBtns.forEach((faqBtn) => {
+	faqBtn.addEventListener('click', (e) => {
+		removeActiveFaqItems();
+		e.target.closest('.faq-item').classList.add('active');
+	});
+});
+
+navBtn.addEventListener('click', handleNav);
+navLinks.forEach((navLink) =>
+	navLink.addEventListener('click', () => {
+		navBtn.classList.remove('open');
+		menu.classList.remove('open');
+		document.body.style.overflow = 'visible';
+		menuOpen = false;
+	})
+);
