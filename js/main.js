5d80fcf302244210a828cccca7d8cd8d887f6d68
Dawid <dawid.marchewczyk@gmail.com>
2023-11-16 21:31:44 +0100

    created offers section

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ const navLinks = document.querySelectorAll('.nav-links__item a');
 const currYear = new Date().getFullYear();
 const footerYear = (document.querySelector('.footer-year').textContent = currYear);
 
+const scrollTopBtn = document.querySelector('.scroll-to-top');
+
 let menuOpen = false;
 
 const handleNav = () => {
@@ -38,6 +40,13 @@ faqBtns.forEach((faqBtn) => {
 	});
 });
 
+window.addEventListener('scroll', () => {
+	if (window.scrollY > 300) {
+		scrollTopBtn.classList.add('active');
+	} else {
+		scrollTopBtn.classList.remove('active');
+	}
+});
 navBtn.addEventListener('click', handleNav);
 navLinks.forEach((navLink) =>
 	navLink.addEventListener('click', () => {
