7362acbf9a59fe9adc2bcbf35a54a4439b0cdb0c
Dawid <dawid.marchewczyk@gmail.com>
2023-11-28 02:37:26 +0100

    updating remote repo

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@ const navBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.nav');
 const navLinks = document.querySelectorAll('.nav-links__item a');
 
-// FOOTER YEAR 
+// FOOTER YEAR
 const currYear = new Date().getFullYear();
 const footerYear = (document.querySelector('.footer-year').textContent = currYear);
-// SCROLL TOP 
+// SCROLL TOP
 const scrollTopBtn = document.querySelector('.scroll-to-top');
 
 const cookieModal = document.querySelector('.cookie-modal');
@@ -19,8 +19,22 @@ const msgStatus = document.querySelector('.msg-status');
 const inputName = document.querySelector('#name');
 const inputEmail = document.querySelector('#email');
 
+// FORM CHECKER
+const formChecker = (input) => {
+	if (input.value.trim().length < 3) {
+		formBtn.disabled = true;
+		formBtn.style.cursor = 'not-allowed'
+	} else {
+		formBtn.disabled = false;
+		formBtn.style.cursor = 'pointer'
+	}
+};
 
-// FORM CHECKER 
+[inputName, inputEmail].forEach(input => input.addEventListener('keyup', () => {
+	formChecker(input)
+}))
+
+
 function isSendForm() {
 	if (document.location.search === '?mail_status=sent') {
 		msgStatus.classList.add('success');
@@ -36,8 +50,7 @@ function isSendForm() {
 		}, 3000);
 	}
 }
-isSendForm()
-
+isSendForm();
 
 // COOKIE
 const cookieHandle = () => {
@@ -110,3 +123,50 @@ showProjectBtns.forEach((item) => {
 		item.parentElement.classList.toggle('show');
 	});
 });
+
+// DARKMODE
+const themeBtn = document.querySelector('.btn-darkMode');
+const moonIco = document.querySelector('.dark');
+const sunIco = document.querySelector('.light');
+const root = document.documentElement;
+let isDark = false;
+
+const toggleTheme = () => {
+	if (!isDark) {
+		// DARK STYLE
+		root.style.setProperty('--white-color', '#1a1a1c');
+		root.style.setProperty('--pure-white-color', '#222');
+		root.style.setProperty('--box-shadow-dark', 'rgba(255, 255, 255, 0.15)');
+
+		root.style.setProperty('--dark-color', '#f9f9f9');
+		root.style.setProperty('--dark-color-light', '#fff');
+		root.style.setProperty('--box-shadow-light', 'rgba(16, 4, 4, 0.15)');
+		isDark = true;
+		moonIco.style.display = 'block';
+		sunIco.style.display = 'none';
+		// CREATING SAVE IN LS
+		localStorage.setItem('theme', 'dark');
+	} else if (isDark) {
+		// LIGHTSTYLE
+		root.style.setProperty('--white-color', '#f9f9f9');
+		root.style.setProperty('--pure-white-color', '#fff');
+		root.style.setProperty('--box-shadow-dark', 'rgba(16, 4, 4, 0.15)');
+
+		root.style.setProperty('--dark-color', '#1a1a1c');
+		root.style.setProperty('--dark-color-light', '#222');
+		root.style.setProperty('--box-shadow-light', 'rgba(255, 255, 255, 0.15)');
+		isDark = false;
+		moonIco.style.display = 'none';
+		sunIco.style.display = 'block';
+		// CREATING SAVE IN LS
+		localStorage.setItem('theme', 'light');
+	}
+};
+
+const checkTheme = () => {
+	localStorage.getItem('theme') === 'dark' ? (isDark = false) : (isDark = true);
+	toggleTheme();
+};
+
+checkTheme();
+themeBtn.addEventListener('click', toggleTheme);
